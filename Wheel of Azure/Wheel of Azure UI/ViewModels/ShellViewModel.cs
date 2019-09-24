1d697cd99d94c79ff07dbc869c1256dc7fd2d6c2
denzalereese <reesedenzale@gmail.com>
2019-09-24 14:38:05 -0700

    sets up WPF and adds start screen

---
+++ Wheel of Azure/Wheel of Azure UI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wheel_of_Azure_UI.ViewModels
+{
+    public class ShellViewModel: Conductor<object>
+    {
+        public ShellViewModel()
+        {
+            ActivateItem(new StartViewModel());
+        }
+    }
+}
---
+++ Wheel of Azure/Wheel of Azure UI/ViewModels/StartViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wheel_of_Azure_UI.ViewModels
+{
+    public class StartViewModel: Screen
+    {
+        public StartViewModel()
+        {
+            System.Media.SoundPlayer player = 
+                new System.Media.SoundPlayer(@"C:\Users\v-deree\Projects\azureRefactor\Wheel of Azure\Wheel of Azure UI\Sounds\theme.wav");
+            player.Play();
+        }
+    }
+}
